a898613c2ea6baa424395cbb464a99e84bb64563
jameskomo <james.komoh@gmail.com>
2019-04-14 20:24:10 +0300

    Requests file created to create pull requests to News API. Json, sources and articles and date imported into request file

--- app/models.py
+++ app/models.py
@@ -13,3 +13,16 @@ class Sources:
         self.language = language
 
 
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+
+    def __init__(self, id, author, title, description, url, image, date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date

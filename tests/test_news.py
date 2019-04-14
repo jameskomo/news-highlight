1bbcf614519491b1ff70c08ae98d0172f30f95f7
jameskomo <james.komoh@gmail.com>
2019-04-14 20:49:12 +0300

    Source testing function created for etsting source instance. Function also created to test variable instances for all source objects

--- tests/test_news.py
+++ tests/test_news.py
@@ -14,4 +14,16 @@ class SourcesTest(unittest.TestCase):
         self.new_source = Sources(
             'CNN', 'CNN News', 'Cable News Newtork that is a leader in providing news worldwide', 'cnn.com', 'general', 'U.S.A', 'en')
 
- 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Sources))
+
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_source.id, 'CNN')
+        self.assertEquals(self.new_source.name, 'CNN News')
+        self.assertEquals(self.new_source.description,
+                          'Cable News Newtork that is a leader in providing news worldwide')
+        self.assertEquals(self.new_source.url, 'cnn.com')
+        self.assertEquals(self.new_source.category, 'general')
+        self.assertEquals(self.new_source.country, 'U.S.A')
+        self.assertEquals(self.new_source.language, 'en')
+
